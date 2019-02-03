1bbc51dc0eb6c726e3febac555e5cab9cd0ca0b0
im-noob <aaravonly4you@gmail.com>
2019-02-03 18:31:55 +0530

    pushing

--- src/Database/DBConnection.java
+++ src/Database/DBConnection.java
@@ -5,11 +5,15 @@
  */
 package Database;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -24,16 +28,55 @@ public class DBConnection {
     
 
     public Connection connectDB(){
+        
         String dbURL = "jdbc:mysql://localhost:3306/hemalayan?zeroDateTimeBehavior=convertToNull";//"jdbc:oracle:thin:@localhost:1521:xe";
         String username = "root";
         String password = "";
         boolean flag =false;
-
+        
+        
+        BufferedReader reader;
+       String str[] = new String[3];
         try {
-
+                int i = 0;
+                reader = new BufferedReader(new FileReader(".env"));
+                String line = reader.readLine();
+                while (line != null) {
+//                        System.out.println(line);
+                        // read next line
+                        str[i++] = line;
+                        line = reader.readLine();
+                }
+                reader.close();
+                System.out.println("URL:"+str[0]);
+                System.out.println("USERNAME:"+str[1]);
+                System.out.println("PASSWORD:"+str[2]);
+//                System.out.print("string:"+str[0]+" username:"+str[1]+" password:"+str[2]);
+                
+                String passArr[] = str[2].split("=");
+                dbURL = str[0].split("=")[1];
+                username = str[1].split("=")[1];
+                System.out.println("Array len:"+passArr.length);
+                if(passArr.length > 1){
+                    password = passArr[1];
+                }else{
+                    password = "";
+                }
+                
+                
+        } catch (Exception e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Error in DB connection");
+                return(null);
+        }
+        
+        try {
+            System.out.println("URL:"+dbURL);
+            System.out.println("USERNAME:"+username);
+            System.out.println("PASSWORD:"+password);
            conn = DriverManager.getConnection(dbURL, username, password);
         } catch (SQLException ex) {
-            System.out.print(ex);
+            System.out.println(ex);
         }
         return conn;
     }
